55f918b73e1d27bc828e847ca6e3be0f2a2435fa
Trier77 <jacob.aa.trier@gmail.com>
2026-05-05 15:52:53 +0200

    navigation til undersider

--- src/pages/StartSide.jsx
+++ src/pages/StartSide.jsx
@@ -1,10 +1,13 @@
 import krop from "../assets/kropudenbg.svg";
 import NavButton from "../components/NavButton";
 import HotspotButton from "../components/HotspotButton";
-import { Navigate } from "react-router";
+import { Navigate, useNavigate } from "react-router";
 import Depression from "./Depression";
 
+
+
 function StartSide() {
+    const navigate = useNavigate();
   return (
     <>
       <section className="flex flex-col items-center ">
@@ -17,12 +20,12 @@ function StartSide() {
       </section>
       <section className="left-0 top-0 relative">        
         <img src={krop} className="w-270 h-480" alt="" />
-        <HotspotButton className="depri-knap left-1/2 top-110  " onClick={() => {Navigate(Depression)}} />
-        <HotspotButton className="ryge-knap left-100 top-230" onClick={() => {}} />
-        <HotspotButton className="kraeft-knap left-170 top-260" onClick={() => {}} />
-        <HotspotButton className="blodsukker-knap left-65 top-300" onClick={() => {}} />
-        <HotspotButton className="gravid-knap left-1/2 top-410" onClick={() => {}} />
-        <HotspotButton className="hormon-knap left-120 top-420" onClick={() => {}} />
+        <HotspotButton className="depri-knap left-1/2 top-110  " onClick={() => navigate("/depression")} />
+        <HotspotButton className="ryge-knap left-100 top-230" onClick={() => navigate("/rygning")} />
+        <HotspotButton className="kraeft-knap left-170 top-260" onClick={() => navigate("/kraeftbehandling")} />
+        <HotspotButton className="blodsukker-knap left-65 top-300" onClick={() => navigate("/blodsukker")} />
+        <HotspotButton className="gravid-knap left-1/2 top-410" onClick={() => navigate("/graviditet")} />
+        <HotspotButton className="hormon-knap left-120 top-420" onClick={() => navigate("/hormoner")} />
         {/* skal knappe komponenterne ligge inden i article */}
         </section>
 
