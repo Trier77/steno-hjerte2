1d70a2a7c5e7f9a9478b6f3380f93b1a261a50e6
Trier77 <jacob.aa.trier@gmail.com>
2026-05-05 10:43:49 +0200

    flex virker men farver virker ikke

--- src/pages/StartSide.jsx
+++ src/pages/StartSide.jsx
@@ -3,7 +3,7 @@ import krop from '../assets/kropudenbg.svg'
 function StartSide(){
     return(
         <>
-        <h1 className="titel z-0 text-amber-300">do you see dis</h1>
+        <h1 className=" decoration-mist-600 decoration-2 text text[#fff]">do you see dis</h1>
         <img src={krop} className="frontbg" width="1920px" height="1080px z-1" alt="" />
         </>
         
