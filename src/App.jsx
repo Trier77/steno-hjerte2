3a3e0eb643367dddf479f37f89b252f81355af2b
Trier77 <jacob.aa.trier@gmail.com>
2026-05-05 09:42:31 +0200

    BrowserRouter/HashRouter setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import nyfront from './assets/nyfront.png'
+import {Routes, Route} from 'react-router'
+import StartSide from './pages/StartSide'
 import './App.css'
 
 function App() {
@@ -9,15 +10,9 @@ function App() {
 
   return (
     <>
-      <section className='top bg-amber-600 bg-cover flex-col'>
-        <h1 className='fornu absolute left-0 italic'  >sup dude jkljkljlk</h1>
-      <img src={nyfront} className="base" width="170" height="179" alt="" />
-      </section>
-      <section className='bund bg-pink-900 bg-cover'>
-        <p>en masse tekst</p>
-        <h1 className='overskrift text-blue-500'>sfilejfoisejfo</h1>
-
-      </section>
+      <Routes>
+        <Route path="/" element={<StartSide />} />
+      </Routes>
     </>
   )
 }
---
+++ src/pages/StartSide.jsx
@@ -0,0 +1,11 @@
+import nyfront from '../assets/nyfront.png'
+
+function StartSide(){
+    return(
+        <h1>do you see dis</h1>
+        
+    )
+}
+
+
+export default StartSide
