c7881cf9958f9feca07e7dfea607853ca4595566
runedrummer81 <runedrummer81@gmail.com>
2026-05-06 13:25:53 +0200

    Updated translations on Rygning

--- src/translations.js
+++ src/translations.js
@@ -5,18 +5,19 @@ const translations = {
         label: "Rygning Historisk",
         heading: "Rygning Historisk",
         intro:
-          "Læger har vidst siden midten af 1900-tallet, at rygning kan give hjertekarsygdom. Mænd har historisk set røget mere end kvinder. I 1950 røg 60% af alle mænd. Det samme gjaldt kun for 10% af kvinderne.",
-        body: 'Derfor blev blodpropper i hjertet opfattet som en mandesygdom eller en "direktørsygdom" fordi det i høj grad var mænd med store cigarer, store maver og manglende motion, der blev ramt.',
+          'Læger har vidst siden midten af 1900-tallet, at rygning øger risikoen for hjerte-kar-sygdom. Mænd har historisk set røget mere end kvinder. Derfor blev blodpropper i hjertet opfattet som en mandesygdom eller "direktørsygdom", fordi det i høj grad var mænd med store cigarer og store maver, der blev ramt.',
+        body: "",
         stats: [
-          { value: "60%", description: "mænd røg i 1950" },
-          { value: "10%", description: "Kvinder røg i 1950" },
+          { value: "75%", description: "af mænd røg i 1950" },
+          { value: "40%", description: "af kvinder røg i 1950" },
         ],
       },
       right: {
-        label: "Rygning i dag",
-        heading: "Rygning i dag",
-        intro: "I dag ryger lige mange kvinder og mænd.",
-        body: 'Og faktisk har det vist sig, at de kvinder der ryger har op mod 25% større risiko for at få en hjertesygdom, end mænd der ryger. Rygning er derfor "farligere" for kvinders hjerter end for mænds. Forskere er ikke klar over, hvad årsagen er.',
+        label: "Rygning i dag og kvinders hjerter",
+        heading: "Rygning i dag og kvinders hjerter",
+        intro:
+          'I dag ryger lige mange kvinder og mænd, men forskning har vist, at rygning er "farligere" for kvinders hjerter.',
+        body: "Hvis en kvinde ryger stiger hendes risiko for en hjertekarsygdom med 25 % mere, end en mands gør.\n\nForskere er ikke klar over, hvad årsagen er.",
         stats: [
           { value: "25%", description: "Større risiko for kvinder end mænd" },
         ],
